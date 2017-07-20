ed9f4e71d3899eefbdc61499cfec1f6b8d872836
Mounic Madiraju <mounicmadiraju@users.noreply.github.com>
2017-07-20 15:04:06 +0800

    Create 404errorpage.py

---
+++ 404errorpage.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import urllib2
+
+baseURL = "https://www.jobstreet.com.my"
+
+for n in xrange(100):
+    fullURL = baseURL + str(n)
+    #print fullURL
+    try:
+        req = urllib2.Request(fullURL)
+        resp = urllib2.urlopen(req)
+        if resp.getcode() == 404:
+            
+            print ("404 Found!")
+        else:
+            
+            print ("URL: {0} Response: {1}".format(fullURL, resp.getcode()))
+    except:
+        print ("Could not connect to URL: {0} ".format(fullURL))
